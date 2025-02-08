0398defb64c47d7ab95f3b1a1ab283205b09defc
jorenham <jhammudoglu@gmail.com>
2025-02-08 15:13:36 +0100

    ✅ remove incorrect static rejection test

--- test/static/reject/modules.pyi
+++ test/static/reject/modules.pyi
@@ -8,7 +8,6 @@ np.warnings  # type: ignore[attr-defined]  # pyright: ignore[reportAttributeAcce
 np.sys  # type: ignore[attr-defined]  # pyright: ignore[reportAttributeAccessIssue]
 np.os  # type: ignore[attr-defined]  # pyright: ignore[reportAttributeAccessIssue]
 np.math  # type: ignore[attr-defined]  # pyright: ignore[reportAttributeAccessIssue]
-np.lib.recfunctions  # type: ignore[attr-defined]  # pyright: ignore[reportAttributeAccessIssue]
 
 np.__deprecated_attrs__  # type: ignore[attr-defined]  # pyright: ignore[reportAttributeAccessIssue]
 np.__expired_functions__  # type: ignore[attr-defined]  # pyright: ignore[reportAttributeAccessIssue]
