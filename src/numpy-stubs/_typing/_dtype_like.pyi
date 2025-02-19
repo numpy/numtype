f109cc708d5fb7ab5689028280b8fc2ffa15780c
jorenham <jhammudoglu@gmail.com>
2025-02-20 00:30:30 +0100

    ♻️ simplified `numpy._typing` dtype- and array-like aliases

--- src/numpy-stubs/_typing/_dtype_like.pyi
+++ src/numpy-stubs/_typing/_dtype_like.pyi
@@ -1,104 +1,69 @@
+from _typeshed import Incomplete
 from collections.abc import Sequence
 from typing import Any, Protocol, TypeAlias, TypedDict, runtime_checkable
-from typing_extensions import TypeVar
+from typing_extensions import Required, TypeVar
 
 import numpy as np
 
 from ._char_codes import (
     _BoolCodes,
-    _ByteCodes,
     _BytesCodes,
-    _CDoubleCodes,
-    _CLongDoubleCodes,
-    _CSingleCodes,
-    _Complex64Codes,
-    _Complex128Codes,
+    _ComplexFloatingCodes,
     _DT64Codes,
-    _DoubleCodes,
-    _Float16Codes,
-    _Float32Codes,
-    _Float64Codes,
-    _HalfCodes,
-    _Int8Codes,
-    _Int16Codes,
-    _Int32Codes,
-    _Int64Codes,
-    _IntCCodes,
-    _IntCodes,
-    _IntPCodes,
-    _LongCodes,
-    _LongDoubleCodes,
-    _LongLongCodes,
+    _FloatingCodes,
+    _NumberCodes,
     _ObjectCodes,
-    _ShortCodes,
-    _SingleCodes,
+    _SignedIntegerCodes,
     _StrCodes,
     _TD64Codes,
-    _UByteCodes,
-    _UInt8Codes,
-    _UInt16Codes,
-    _UInt32Codes,
-    _UInt64Codes,
-    _UIntCCodes,
-    _UIntCodes,
-    _UIntPCodes,
-    _ULongLongCodes,
-    _UShortCodes,
+    _UnsignedIntegerCodes,
     _VoidCodes,
 )
 from ._shape import _ShapeLike
 
-_SCT = TypeVar("_SCT", bound=np.generic)
-_DType_co = TypeVar("_DType_co", covariant=True, bound=np.dtype[Any])
+_ScalarT = TypeVar("_ScalarT", bound=np.generic)
+_DTypeT_co = TypeVar("_DTypeT_co", covariant=True, bound=np.dtype[Any])
 
-_DTypeLikeNested: TypeAlias = Any
+# TODO(jorenham): Actually annotate this
+_DTypeLikeNested: TypeAlias = Incomplete
 
-# Mandatory keys
-class _DTypeDictBase(TypedDict):
-    names: Sequence[str]
-    formats: Sequence[_DTypeLikeNested]
-
-# Mandatory + optional keys
-class _DTypeDict(_DTypeDictBase, total=False):
-    # Only `str` elements are usable as indexing aliases,
-    # but `titles` can in principle accept any object
-    offsets: Sequence[int]
-    titles: Sequence[Any]
+class _DTypeDict(TypedDict, total=False):
+    names: Required[Sequence[str]]
+    formats: Required[Sequence[_DTypeLikeNested]]
     itemsize: int
     aligned: bool
+    offsets: Sequence[int]
+    # Only `str` elements are usable as indexing aliases,
+    # but `titles` can in principle accept any object
+    titles: Sequence[str | Any]
 
 # A protocol for anything with the dtype attribute
 @runtime_checkable
-class _SupportsDType(Protocol[_DType_co]):
+class _SupportsDType(Protocol[_DTypeT_co]):
     @property
-    def dtype(self) -> _DType_co: ...
+    def dtype(self) -> _DTypeT_co: ...
 
 # A subset of `npt.DTypeLike` that can be parametrized w.r.t. `np.generic`
-_DTypeLike: TypeAlias = np.dtype[_SCT] | type[_SCT] | _SupportsDType[np.dtype[_SCT]]
+_DTypeLike: TypeAlias = type[_ScalarT] | np.dtype[_ScalarT] | _SupportsDType[np.dtype[_ScalarT]]
 
 # Would create a dtype[np.void]
-_VoidDTypeLike: TypeAlias = (
-    tuple[_DTypeLikeNested, int]
-    | tuple[_DTypeLikeNested, _ShapeLike]
-    | list[Any]
-    | _DTypeDict
-    | tuple[_DTypeLikeNested, _DTypeLikeNested]
-)
+_VoidDTypeLike: TypeAlias = tuple[_DTypeLikeNested, int | _ShapeLike | _DTypeLikeNested] | list[Any] | _DTypeDict
 
-# Anything that can be coerced into numpy.dtype.
-# Reference: https://docs.scipy.org/doc/numpy/reference/arrays.dtypes.html
-DTypeLike: TypeAlias = (
-    np.dtype[Any]
-    # array-scalar types and generic types
-    | type[Any]  # NOTE: We're stuck with `type[Any]` due to object dtypes
-    # anything with a dtype attribute
-    | _SupportsDType[np.dtype[Any]]
-    # character codes, type strings or comma-separated fields, e.g., 'float64'
-    | str
-    | _VoidDTypeLike
-    # default data type (float64)
-    | None
-)
+# Aliases for commonly used dtype-like objects.
+# Note that the precision of `np.number` subclasses is ignored herein.
+_DTypeLikeBool: TypeAlias = type[bool] | _DTypeLike[np.bool] | _BoolCodes
+_DTypeLikeUInt: TypeAlias = _DTypeLike[np.unsignedinteger] | _UnsignedIntegerCodes
+_DTypeLikeInt: TypeAlias = type[int] | _DTypeLike[np.signedinteger] | _SignedIntegerCodes
+_DTypeLikeFloat: TypeAlias = type[float] | _DTypeLike[np.floating] | _FloatingCodes
+_DTypeLikeComplex: TypeAlias = type[complex] | _DTypeLike[np.complexfloating] | _ComplexFloatingCodes
+_DTypeLikeDT64: TypeAlias = _DTypeLike[np.timedelta64] | _TD64Codes
+_DTypeLikeTD64: TypeAlias = _DTypeLike[np.datetime64] | _DT64Codes
+_DTypeLikeStr: TypeAlias = type[str] | _DTypeLike[np.str_] | _StrCodes
+_DTypeLikeBytes: TypeAlias = type[bytes] | _DTypeLike[np.bytes_] | _BytesCodes
+_DTypeLikeVoid: TypeAlias = _DTypeLike[np.void] | _VoidCodes | _VoidDTypeLike
+_DTypeLikeObject: TypeAlias = type | _DTypeLike[np.object_] | _ObjectCodes
+
+_DTypeLikeComplex_co: TypeAlias = type[complex] | _DTypeLike[np.number] | _NumberCodes | _BoolCodes
 
 # NOTE: while it is possible to provide the dtype as a dict of
 # dtype-like objects (e.g. `{'field1': ..., 'field2': ..., ...}`),
@@ -107,75 +72,6 @@ DTypeLike: TypeAlias = (
 #
 # See https://github.com/numpy/numpy/issues/16891 for more details.
 
-# Aliases for commonly used dtype-like objects.
-# Note that the precision of `np.number` subclasses is ignored herein.
-_DTypeLikeBool: TypeAlias = type[bool] | type[np.bool] | np.dtype[np.bool] | _SupportsDType[np.dtype[np.bool]] | _BoolCodes
-_DTypeLikeUInt: TypeAlias = (
-    type[np.unsignedinteger[Any]]
-    | np.dtype[np.unsignedinteger[Any]]
-    | _SupportsDType[np.dtype[np.unsignedinteger[Any]]]
-    | _UInt8Codes
-    | _UInt16Codes
-    | _UInt32Codes
-    | _UInt64Codes
-    | _UByteCodes
-    | _UShortCodes
-    | _UIntCCodes
-    | _LongCodes
-    | _ULongLongCodes
-    | _UIntPCodes
-    | _UIntCodes
-)
-_DTypeLikeInt: TypeAlias = (
-    type[int]
-    | type[np.signedinteger[Any]]
-    | np.dtype[np.signedinteger[Any]]
-    | _SupportsDType[np.dtype[np.signedinteger[Any]]]
-    | _Int8Codes
-    | _Int16Codes
-    | _Int32Codes
-    | _Int64Codes
-    | _ByteCodes
-    | _ShortCodes
-    | _IntCCodes
-    | _LongCodes
-    | _LongLongCodes
-    | _IntPCodes
-    | _IntCodes
-)
-_DTypeLikeFloat: TypeAlias = (
-    type[float]
-    | type[np.floating[Any]]
-    | np.dtype[np.floating[Any]]
-    | _SupportsDType[np.dtype[np.floating[Any]]]
-    | _Float16Codes
-    | _Float32Codes
-    | _Float64Codes
-    | _HalfCodes
-    | _SingleCodes
-    | _DoubleCodes
-    | _LongDoubleCodes
-)
-_DTypeLikeComplex: TypeAlias = (
-    type[complex]
-    | type[np.complexfloating[Any]]
-    | np.dtype[np.complexfloating[Any]]
-    | _SupportsDType[np.dtype[np.complexfloating[Any]]]
-    | _Complex64Codes
-    | _Complex128Codes
-    | _CSingleCodes
-    | _CDoubleCodes
-    | _CLongDoubleCodes
-)
-_DTypeLikeDT64: TypeAlias = (
-    type[np.timedelta64] | np.dtype[np.timedelta64] | _SupportsDType[np.dtype[np.timedelta64]] | _TD64Codes
-)
-_DTypeLikeTD64: TypeAlias = type[np.datetime64] | np.dtype[np.datetime64] | _SupportsDType[np.dtype[np.datetime64]] | _DT64Codes
-_DTypeLikeStr: TypeAlias = type[str] | type[np.str_] | np.dtype[np.str_] | _SupportsDType[np.dtype[np.str_]] | _StrCodes
-_DTypeLikeBytes: TypeAlias = (
-    type[bytes] | type[np.bytes_] | np.dtype[np.bytes_] | _SupportsDType[np.dtype[np.bytes_]] | _BytesCodes
-)
-_DTypeLikeVoid: TypeAlias = type[np.void] | np.dtype[np.void] | _SupportsDType[np.dtype[np.void]] | _VoidCodes | _VoidDTypeLike
-_DTypeLikeObject: TypeAlias = type | np.dtype[np.object_] | _SupportsDType[np.dtype[np.object_]] | _ObjectCodes
-
-_DTypeLikeComplex_co: TypeAlias = _DTypeLikeBool | _DTypeLikeUInt | _DTypeLikeInt | _DTypeLikeFloat | _DTypeLikeComplex
+# Anything that can be coerced into numpy.dtype.
+# Reference: https://docs.scipy.org/doc/numpy/reference/arrays.dtypes.html
+DTypeLike: TypeAlias = str | _DTypeLike[Any] | _VoidDTypeLike | None
