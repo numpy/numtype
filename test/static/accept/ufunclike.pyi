eb4a32e3aea22e9f3efe1c000ecaa3f2dd5b41f9
jorenham <jhammudoglu@gmail.com>
2025-02-23 00:32:06 +0100

    ✨ fix and improve `numpy.lib._utils_impl`

--- test/static/accept/ufunclike.pyi
+++ test/static/accept/ufunclike.pyi
@@ -1,31 +1,36 @@
-from typing_extensions import assert_type
+from typing import TypeAlias
+from typing_extensions import TypeVar, assert_type
 
 import numpy as np
 import numpy.typing as npt
 
+AR_u: npt.NDArray[np.uint32]
+AR_U: npt.NDArray[np.str_]
 AR_LIKE_b: list[bool]
-AR_LIKE_u: list[np.uint32]
 AR_LIKE_i: list[int]
 AR_LIKE_f: list[float]
-AR_LIKE_O: list[np.object_]
 
-AR_U: npt.NDArray[np.str_]
+###
 
-assert_type(np.fix(AR_LIKE_b), npt.NDArray[np.floating])
-assert_type(np.fix(AR_LIKE_u), npt.NDArray[np.floating])
-assert_type(np.fix(AR_LIKE_i), npt.NDArray[np.floating])
-assert_type(np.fix(AR_LIKE_f), npt.NDArray[np.floating])
-assert_type(np.fix(AR_LIKE_O), npt.NDArray[np.object_])
+_ScalarT = TypeVar("_ScalarT", bound=np.generic)
+_Array1D: TypeAlias = np.ndarray[tuple[int], np.dtype[_ScalarT]]
+
+###
+
+assert_type(np.fix(AR_u), npt.NDArray[np.uint32])
+assert_type(np.fix(AR_LIKE_b), npt.NDArray[np.bool])
+assert_type(np.fix(AR_LIKE_i), npt.NDArray[np.intp])
+assert_type(np.fix(AR_LIKE_f), npt.NDArray[np.float64])
 assert_type(np.fix(AR_LIKE_f, out=AR_U), npt.NDArray[np.str_])
 
-assert_type(np.isposinf(AR_LIKE_b), npt.NDArray[np.bool])
-assert_type(np.isposinf(AR_LIKE_u), npt.NDArray[np.bool])
-assert_type(np.isposinf(AR_LIKE_i), npt.NDArray[np.bool])
-assert_type(np.isposinf(AR_LIKE_f), npt.NDArray[np.bool])
+assert_type(np.isposinf(AR_u), npt.NDArray[np.bool])
+assert_type(np.isposinf(AR_LIKE_b), _Array1D[np.bool])
+assert_type(np.isposinf(AR_LIKE_i), _Array1D[np.bool])
+assert_type(np.isposinf(AR_LIKE_f), _Array1D[np.bool])
 assert_type(np.isposinf(AR_LIKE_f, out=AR_U), npt.NDArray[np.str_])
 
-assert_type(np.isneginf(AR_LIKE_b), npt.NDArray[np.bool])
-assert_type(np.isneginf(AR_LIKE_u), npt.NDArray[np.bool])
-assert_type(np.isneginf(AR_LIKE_i), npt.NDArray[np.bool])
-assert_type(np.isneginf(AR_LIKE_f), npt.NDArray[np.bool])
+assert_type(np.isneginf(AR_u), npt.NDArray[np.bool])
+assert_type(np.isneginf(AR_LIKE_b), _Array1D[np.bool])
+assert_type(np.isneginf(AR_LIKE_i), _Array1D[np.bool])
+assert_type(np.isneginf(AR_LIKE_f), _Array1D[np.bool])
 assert_type(np.isneginf(AR_LIKE_f, out=AR_U), npt.NDArray[np.str_])
