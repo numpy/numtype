e1e7d8db1278228bbda7fab0883ecc92216f6f3e
Joren Hammudoglu <jhammudoglu@gmail.com>
2025-02-24 17:43:50 +0100

    🩹 add missing implicit submodule re-exports to `numpy.lib` (#182)

--- src/numpy-stubs/lib/__init__.pyi
+++ src/numpy-stubs/lib/__init__.pyi
@@ -1,7 +1,35 @@
 from numpy._core.function_base import add_newdoc
 from numpy._core.multiarray import add_docstring, tracemalloc_domain
 
-from . import array_utils, format, introspect, mixins, npyio, scimath, stride_tricks  # noqa: F401
+from . import (
+    _array_utils_impl as _array_utils_impl,
+    _arraypad_impl as _arraypad_impl,
+    _arraysetops_impl as _arraysetops_impl,
+    _arrayterator_impl as _arrayterator_impl,
+    _datasource as _datasource,
+    _function_base_impl as _function_base_impl,
+    _histograms_impl as _histograms_impl,
+    _index_tricks_impl as _index_tricks_impl,
+    _iotools as _iotools,
+    _nanfunctions_impl as _nanfunctions_impl,
+    _npyio_impl as _npyio_impl,
+    _polynomial_impl as _polynomial_impl,
+    _scimath_impl as _scimath_impl,
+    _shape_base_impl as _shape_base_impl,
+    _stride_tricks_impl as _stride_tricks_impl,
+    _twodim_base_impl as _twodim_base_impl,
+    _type_check_impl as _type_check_impl,
+    _ufunclike_impl as _ufunclike_impl,
+    _utils_impl as _utils_impl,
+    _version as _version,
+    array_utils,
+    format as format,
+    introspect,
+    mixins,
+    npyio,
+    scimath,
+    stride_tricks,
+)
 from ._arrayterator_impl import Arrayterator
 from ._version import NumpyVersion
 
