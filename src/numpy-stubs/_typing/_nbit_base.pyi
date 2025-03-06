51c17068512162ca6b944b120d4623b48954a746
Joren Hammudoglu <jhammudoglu@gmail.com>
2025-03-06 06:48:35 +0100

    🏷️ fix stubtest error in `numpy.typing` (#260)

--- src/numpy-stubs/_typing/_nbit_base.pyi
+++ src/numpy-stubs/_typing/_nbit_base.pyi
@@ -1,7 +1,11 @@
+from typing import final
+
+@final
 class NBitBase: ...
-class _128Bit(NBitBase): ...  # undocumented
-class _96Bit(_128Bit): ...  # undocumented
-class _64Bit(_96Bit): ...  # undocumented
-class _32Bit(_64Bit): ...  # undocumented
-class _16Bit(_32Bit): ...  # undocumented
-class _8Bit(_16Bit): ...  # undocumented
+
+class _128Bit(NBitBase): ...  # type: ignore[misc]  # pyright: ignore[reportGeneralTypeIssues]
+class _96Bit(_128Bit): ...  # type: ignore[misc]
+class _64Bit(_96Bit): ...  # type: ignore[misc]
+class _32Bit(_64Bit): ...  # type: ignore[misc]
+class _16Bit(_32Bit): ...  # type: ignore[misc]
+class _8Bit(_16Bit): ...  # type: ignore[misc]
