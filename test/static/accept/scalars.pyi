29ca9b4a1524e26b7fcb5bebbb7c133170e9bf5d
jorenham <jhammudoglu@gmail.com>
2025-03-06 13:35:24 +0100

    🐛 fix py310 syntax error in a test

--- test/static/accept/scalars.pyi
+++ test/static/accept/scalars.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Literal, TypeAlias
+from typing import Any, Literal, TypeAlias, Unpack
 from typing_extensions import assert_type
 
 import numpy as np
@@ -121,7 +121,7 @@ assert_type(
     S.reshape(1, 1, 1, 1, 1),
     np.ndarray[
         # len(shape) >= 5
-        tuple[_1, _1, _1, _1, _1, *tuple[_1, ...]],
+        tuple[_1, _1, _1, _1, _1, Unpack[tuple[_1, ...]]],
         np.dtype[np.bytes_],
     ],
 )
