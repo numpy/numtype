d8593377c273b55d546038aa4fb64d74f34f1276
jorenham <jhammudoglu@gmail.com>
2025-03-07 18:09:00 +0100

    🏷️ stub `numpy.f2py.f2py2e`

--- src/numpy-stubs/f2py/__init__.pyi
+++ src/numpy-stubs/f2py/__init__.pyi
@@ -1,17 +1,5 @@
-from collections.abc import Iterable
-from typing import TypedDict, type_check_only
+from .f2py2e import main as main, run_main
 
 __all__ = ["get_include", "run_main"]
 
-@type_check_only
-class _F2PyDictBase(TypedDict):
-    csrc: list[str]
-    h: list[str]
-
-@type_check_only
-class _F2PyDict(_F2PyDictBase, total=False):
-    fsrc: list[str]
-    ltx: list[str]
-
 def get_include() -> str: ...
-def run_main(comline_list: Iterable[str]) -> dict[str, _F2PyDict]: ...
