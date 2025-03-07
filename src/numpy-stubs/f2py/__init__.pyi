fabf3f83eeedc86e8b25bd604befb24fee6dda69
jorenham <jhammudoglu@gmail.com>
2025-03-07 15:09:59 +0100

    🐛 remove spurious `f2py.compile` function

--- src/numpy-stubs/f2py/__init__.pyi
+++ src/numpy-stubs/f2py/__init__.pyi
@@ -1,7 +1,5 @@
-import subprocess
-from _typeshed import StrOrBytesPath
 from collections.abc import Iterable
-from typing import Literal as L, TypedDict, overload, type_check_only
+from typing import TypedDict, type_check_only
 
 __all__ = ["get_include", "run_main"]
 
@@ -15,31 +13,5 @@ class _F2PyDict(_F2PyDictBase, total=False):
     fsrc: list[str]
     ltx: list[str]
 
-#
 def get_include() -> str: ...
-
-#
 def run_main(comline_list: Iterable[str]) -> dict[str, _F2PyDict]: ...
-
-#
-@overload
-def compile(
-    source: str | bytes,
-    modulename: str = ...,
-    extra_args: str | list[str] = ...,
-    verbose: bool = ...,
-    source_fn: StrOrBytesPath | None = ...,
-    extension: L[".f", ".f90"] = ...,
-    full_output: L[False] = ...,
-) -> int: ...
-@overload
-def compile(
-    source: str | bytes,
-    modulename: str = ...,
-    extra_args: str | list[str] = ...,
-    verbose: bool = ...,
-    source_fn: StrOrBytesPath | None = ...,
-    extension: L[".f", ".f90"] = ...,
-    *,
-    full_output: L[True],
-) -> subprocess.CompletedProcess[bytes]: ...
