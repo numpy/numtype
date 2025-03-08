bcf19f0b279cd8e87258c5151ce2f29a735ae14b
GUAN MING <guanmingchiu@gmail.com>
2025-03-09 00:28:51 +0800

    🩹 rename CDataVoidPointer to _CDataVoidPointer

--- src/numpy-stubs/random/_common.pyi
+++ src/numpy-stubs/random/_common.pyi
@@ -5,12 +5,12 @@ import numpy as np
 
 __all__: list[str] = ["interface"]
 
-CDataVoidPointer: TypeAlias = Any
+_CDataVoidPointer: TypeAlias = Any
 
 class interface(NamedTuple):
     state_address: int
-    state: CDataVoidPointer
+    state: _CDataVoidPointer
     next_uint64: Callable[..., np.uint64]
     next_uint32: Callable[..., np.uint32]
     next_double: Callable[..., np.float64]
-    bit_generator: CDataVoidPointer
+    bit_generator: _CDataVoidPointer
