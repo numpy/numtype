e5b9734ed8a19c7198f8bc9400b45264300860f4
GUAN MING <guanmingchiu@gmail.com>
2025-03-09 00:22:15 +0800

    🦾 enhance readability by add CDataVoidPointer
    Co-Authored-By: Joren Hammudoglu <jhammudoglu@gmail.com>

--- src/numpy-stubs/random/_common.pyi
+++ src/numpy-stubs/random/_common.pyi
@@ -1,14 +1,16 @@
 from collections.abc import Callable
-from typing import Any, NamedTuple
+from typing import Any, NamedTuple, TypeAlias
 
 import numpy as np
 
 __all__: list[str] = ["interface"]
 
+CDataVoidPointer: TypeAlias = Any
+
 class interface(NamedTuple):
     state_address: int
-    state: Any
+    state: CDataVoidPointer
     next_uint64: Callable[..., np.uint64]
     next_uint32: Callable[..., np.uint32]
     next_double: Callable[..., np.float64]
-    bit_generator: Any
+    bit_generator: CDataVoidPointer
