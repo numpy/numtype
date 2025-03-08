3f44b76d0d6cf412797b95ce96b99d04df53d6e3
jorenham <jhammudoglu@gmail.com>
2025-03-08 09:19:05 +0100

    🏷️ stub `numpy.f2py._backends.*`

---
+++ src/numpy-stubs/f2py/_backends/_distutils.pyi
@@ -0,0 +1,11 @@
+from typing_extensions import deprecated
+
+from ._backend import Backend
+
+class DistutilsBackend(Backend):
+    @deprecated(
+        "distutils has been deprecated since NumPy 1.26.x. Use the Meson backend instead, or generate wrappers without -c and "
+        "use a custom build script"
+    )
+    def __init__(sef, /, *args: object, **kwargs: object) -> None: ...
+    def compile(self) -> None: ...
