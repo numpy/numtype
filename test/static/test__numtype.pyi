032ad6434a41aaab14e4dcaef39a62d2e09e658f
jorenham <jhammudoglu@gmail.com>
2025-03-10 04:56:07 +0100

    ✅ test `_numtype._ToArray1_0d` for all bare scalar types

--- test/static/test__numtype.pyi
+++ test/static/test__numtype.pyi
@@ -21,6 +21,32 @@ U_: str
 O_: object
 
 ###
+# _ToArray1_0d
+
+can_b1_0d: _nt._ToArray1_0d[np.bool] = np.bool()
+can_i1_0d: _nt._ToArray1_0d[np.int8] = np.int8()
+can_i2_0d: _nt._ToArray1_0d[np.int16] = np.int16()
+can_i4_0d: _nt._ToArray1_0d[np.int32] = np.int32()
+can_i8_0d: _nt._ToArray1_0d[np.int64] = np.int64()
+can_u1_0d: _nt._ToArray1_0d[np.uint8] = np.uint8()
+can_u2_0d: _nt._ToArray1_0d[np.uint16] = np.uint16()
+can_u4_0d: _nt._ToArray1_0d[np.uint32] = np.uint32()
+can_u8_0d: _nt._ToArray1_0d[np.uint64] = np.uint64()
+can_f2_0d: _nt._ToArray1_0d[np.float16] = np.float16()
+can_f4_0d: _nt._ToArray1_0d[np.float32] = np.float32()
+can_f8_0d: _nt._ToArray1_0d[np.float64] = np.float64()
+can_ld_0d: _nt._ToArray1_0d[np.longdouble] = np.longdouble()
+can_c8_0d: _nt._ToArray1_0d[np.complex64] = np.complex64()
+can_c16_0d: _nt._ToArray1_0d[np.complex128] = np.complex128()
+can_cld_0d: _nt._ToArray1_0d[np.clongdouble] = np.clongdouble()
+can_O_0d: _nt._ToArray1_0d[np.object_] = np.empty((), np.object_)
+can_M_0d: _nt._ToArray1_0d[np.datetime64] = np.datetime64(None)
+can_m_0d: _nt._ToArray1_0d[np.timedelta64] = np.timedelta64(None)
+can_S_0d: _nt._ToArray1_0d[np.bytes_] = np.bytes_(b"")
+can_U_0d: _nt._ToArray1_0d[np.str_] = np.str_("")
+can_V_0d: _nt._ToArray1_0d[np.void] = np.void(b"")
+
+###
 # bool
 
 b1: np.bool
