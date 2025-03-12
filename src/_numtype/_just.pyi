6daea3fe59923e31affd055bd505d80bd7da8f18
jorenham <jhammudoglu@gmail.com>
2025-03-12 01:26:50 +0100

    ✨ more and cleaner `_numtype.Just*` types

--- src/_numtype/_just.pyi
+++ src/_numtype/_just.pyi
@@ -1,7 +1,11 @@
+# mypy: disable-error-code=override
+# pyright: reportIncompatibleMethodOverride=false
+
+import datetime as dt
 from typing import Protocol, SupportsFloat, SupportsIndex, TypeAlias, final, type_check_only
 from typing_extensions import Self, TypeVar
 
-__all__ = ["Just", "JustComplex", "JustFloat", "JustInt"]
+__all__ = ["Just", "JustBytes", "JustComplex", "JustFloat", "JustInt", "JustObject", "JustStr"]
 
 ###
 
@@ -14,28 +18,77 @@ _ToFloat: TypeAlias = SupportsFloat | SupportsIndex
 # Type-check-only equivalents of the `optype.Just*` types, see
 # https://github.com/jorenham/optype#just
 
+# NOTE: These should only be used to annotate _input_, never _output_.
+
 @final  # the pyright and mypy errors are false positives because of this @final
 @type_check_only
 class Just(Protocol[_T]):
-    @property  # type: ignore[override]
-    def __class__(self, /) -> type[_T]: ...  # pyright: ignore[reportIncompatibleMethodOverride]
+    @property
+    def __class__(self, /) -> type[_T]: ...
     @__class__.setter
     def __class__(self, t: type[_T], /) -> None: ...
 
 @final
 @type_check_only
-class JustInt(Just[int], Protocol):  # type: ignore[misc]  # pyright: ignore[reportGeneralTypeIssues]
+class JustInt(Protocol):
+    @property
+    def __class__(self, /) -> type[int]: ...
+    @__class__.setter
+    def __class__(self, t: type[int], /) -> None: ...
+
     # workaround for `pyright<1.1.390` and `basedpyright<1.22.1`
     def __new__(cls, x: str | bytes | bytearray, /, base: SupportsIndex) -> Self: ...
 
 @final
 @type_check_only
-class JustFloat(Just[float], Protocol):  # type: ignore[misc]  # pyright: ignore[reportGeneralTypeIssues]
+class JustFloat(Protocol):
+    @property
+    def __class__(self, /) -> type[float]: ...
+    @__class__.setter
+    def __class__(self, t: type[float], /) -> None: ...
+
     # workaround for `pyright<1.1.390` and `basedpyright<1.22.1`
     def hex(self, /) -> str: ...
 
 @final
 @type_check_only
-class JustComplex(Just[complex], Protocol):  # type: ignore[misc]  # pyright: ignore[reportGeneralTypeIssues]
+class JustComplex(Protocol):
+    @property
+    def __class__(self, /) -> type[complex]: ...
+    @__class__.setter
+    def __class__(self, t: type[complex], /) -> None: ...
+
     # workaround for `pyright<1.1.390` and `basedpyright<1.22.1`
     def __new__(cls, /, real: _ToFloat, imag: _ToFloat) -> Self: ...
+
+@final
+@type_check_only
+class JustBytes(Protocol):
+    @property
+    def __class__(self, /) -> type[bytes]: ...
+    @__class__.setter
+    def __class__(self, t: type[bytes], /) -> None: ...
+
+@final
+@type_check_only
+class JustStr(Protocol):
+    @property
+    def __class__(self, /) -> type[str]: ...
+    @__class__.setter
+    def __class__(self, t: type[str], /) -> None: ...
+
+@final
+@type_check_only
+class JustDate(Protocol):
+    @property
+    def __class__(self, /) -> type[dt.date]: ...
+    @__class__.setter
+    def __class__(self, t: type[dt.date], /) -> None: ...
+
+@final
+@type_check_only
+class JustObject(Protocol):
+    @property
+    def __class__(self, /) -> type[object]: ...
+    @__class__.setter
+    def __class__(self, t: type[object], /) -> None: ...
