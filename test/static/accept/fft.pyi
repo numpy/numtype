0bce91044a15c6f51d88701dd8f84abbd9cf3988
jorenham <jhammudoglu@gmail.com>
2025-03-20 19:00:30 +0100

    ✨ shape-typing and improved dtypes for `numpy.fft.[r]fftfreq`

--- test/static/accept/fft.pyi
+++ test/static/accept/fft.pyi
@@ -1,37 +1,86 @@
+from typing import TypeAlias, TypeVar
 from typing_extensions import assert_type
 
 import numpy as np
 import numpy.typing as npt
 
-AR_f8: npt.NDArray[np.float64]
-AR_c16: npt.NDArray[np.complex128]
-AR_LIKE_f8: list[float]
+###
 
-assert_type(np.fft.fftshift(AR_f8), npt.NDArray[np.float64])
-assert_type(np.fft.fftshift(AR_LIKE_f8, axes=0), npt.NDArray[np.floating])
+_ShapeT = TypeVar("_ShapeT", bound=tuple[int, ...])
+_ScalarT = TypeVar("_ScalarT", bound=np.generic)
+Array: TypeAlias = np.ndarray[_ShapeT, np.dtype[_ScalarT]]
+Array1D: TypeAlias = Array[tuple[int], _ScalarT]
 
-assert_type(np.fft.ifftshift(AR_f8), npt.NDArray[np.float64])
-assert_type(np.fft.ifftshift(AR_LIKE_f8, axes=0), npt.NDArray[np.floating])
+###
 
-assert_type(np.fft.fftfreq(5, AR_f8), npt.NDArray[np.floating])
-assert_type(np.fft.fftfreq(np.int64(), AR_c16), npt.NDArray[np.inexact])
+f0: float
+f0_1d: list[float]
 
-assert_type(np.fft.rfftfreq(5, AR_f8), npt.NDArray[np.floating])
-assert_type(np.fft.rfftfreq(np.int64(), AR_c16), npt.NDArray[np.inexact])
+f8_1d: Array1D[np.float64]
+f8_nd: npt.NDArray[np.float64]
+f16_1d: Array1D[np.longdouble]
+f16_nd: npt.NDArray[np.longdouble]
 
-assert_type(np.fft.fft(AR_f8), npt.NDArray[np.complex128])
-assert_type(np.fft.ifft(AR_f8, axis=1), npt.NDArray[np.complex128])
-assert_type(np.fft.rfft(AR_f8, n=None), npt.NDArray[np.complex128])
-assert_type(np.fft.irfft(AR_f8, norm="ortho"), npt.NDArray[np.float64])
-assert_type(np.fft.hfft(AR_f8, n=2), npt.NDArray[np.float64])
-assert_type(np.fft.ihfft(AR_f8), npt.NDArray[np.complex128])
+c8_1d: Array1D[np.complex64]
+c8_nd: npt.NDArray[np.complex64]
+c16_1d: Array1D[np.complex128]
+c16_nd: npt.NDArray[np.complex128]
+c32_1d: Array1D[np.clongdouble]
+c32_nd: npt.NDArray[np.clongdouble]
 
-assert_type(np.fft.fftn(AR_f8), npt.NDArray[np.complex128])
-assert_type(np.fft.ifftn(AR_f8), npt.NDArray[np.complex128])
-assert_type(np.fft.rfftn(AR_f8), npt.NDArray[np.complex128])
-assert_type(np.fft.irfftn(AR_f8), npt.NDArray[np.float64])
+###
 
-assert_type(np.fft.rfft2(AR_f8), npt.NDArray[np.complex128])
-assert_type(np.fft.ifft2(AR_f8), npt.NDArray[np.complex128])
-assert_type(np.fft.fft2(AR_f8), npt.NDArray[np.complex128])
-assert_type(np.fft.irfft2(AR_f8), npt.NDArray[np.float64])
+# keep in sync with `ifftshift` (same signature)
+assert_type(np.fft.fftshift(f8_nd), npt.NDArray[np.float64])
+assert_type(np.fft.fftshift(f0_1d, axes=0), npt.NDArray[np.floating])
+
+assert_type(np.fft.ifftshift(f8_nd), npt.NDArray[np.float64])
+assert_type(np.fft.ifftshift(f0_1d, axes=0), npt.NDArray[np.floating])
+
+###
+
+# keep in sync with `rfftfreq` (same signature)
+assert_type(np.fft.fftfreq(5, 0.1), Array1D[np.float64])
+assert_type(np.fft.fftfreq(5, f8_1d), Array1D[np.float64])
+assert_type(np.fft.fftfreq(5, f16_1d), Array1D[np.longdouble])
+assert_type(np.fft.fftfreq(5, 0.1j), Array1D[np.complex128])
+assert_type(np.fft.fftfreq(5, c8_1d), Array1D[np.complex128])
+assert_type(np.fft.fftfreq(5, c16_1d), Array1D[np.complex128])
+assert_type(np.fft.fftfreq(5, c32_1d), Array1D[np.clongdouble])
+assert_type(np.fft.fftfreq(5, f8_nd), npt.NDArray[np.float64])
+assert_type(np.fft.fftfreq(5, f16_nd), npt.NDArray[np.longdouble])
+assert_type(np.fft.fftfreq(5, c8_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.fftfreq(5, c16_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.fftfreq(5, c32_nd), npt.NDArray[np.clongdouble])
+
+assert_type(np.fft.rfftfreq(5, 0.1), Array1D[np.float64])
+assert_type(np.fft.rfftfreq(5, f8_1d), Array1D[np.float64])
+assert_type(np.fft.rfftfreq(5, f16_1d), Array1D[np.longdouble])
+assert_type(np.fft.rfftfreq(5, 0.1j), Array1D[np.complex128])
+assert_type(np.fft.rfftfreq(5, c8_1d), Array1D[np.complex128])
+assert_type(np.fft.rfftfreq(5, c16_1d), Array1D[np.complex128])
+assert_type(np.fft.rfftfreq(5, c32_1d), Array1D[np.clongdouble])
+assert_type(np.fft.rfftfreq(5, f8_nd), npt.NDArray[np.float64])
+assert_type(np.fft.rfftfreq(5, f16_nd), npt.NDArray[np.longdouble])
+assert_type(np.fft.rfftfreq(5, c8_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.rfftfreq(5, c16_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.rfftfreq(5, c32_nd), npt.NDArray[np.clongdouble])
+
+###
+
+assert_type(np.fft.fft(f8_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.ifft(f8_nd, axis=1), npt.NDArray[np.complex128])
+assert_type(np.fft.rfft(f8_nd, n=None), npt.NDArray[np.complex128])
+assert_type(np.fft.irfft(f8_nd, norm="ortho"), npt.NDArray[np.float64])
+assert_type(np.fft.hfft(f8_nd, n=2), npt.NDArray[np.float64])
+assert_type(np.fft.ihfft(f8_nd), npt.NDArray[np.complex128])
+
+assert_type(np.fft.fftn(f8_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.ifftn(f8_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.rfftn(f8_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.irfftn(f8_nd), npt.NDArray[np.float64])
+
+assert_type(np.fft.rfft2(f8_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.ifft2(f8_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.fft2(f8_nd), npt.NDArray[np.complex128])
+assert_type(np.fft.irfft2(f8_nd), npt.NDArray[np.float64])
