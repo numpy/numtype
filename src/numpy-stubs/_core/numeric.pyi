3109f1c237981f748df366d2e8af3116521cd69c
GUAN MING <guanmingchiu@gmail.com>
2025-03-22 22:38:15 +0800

    🐛 implicit export for normalize_axis_tuple

--- src/numpy-stubs/_core/numeric.pyi
+++ src/numpy-stubs/_core/numeric.pyi
@@ -79,6 +79,7 @@ from _numtype._just import JustComplex
 from numpy import _AnyShapeT, _OrderCF, _OrderKACF, ufunc  # noqa: ICN003
 from numpy._typing import ArrayLike, DTypeLike, _ArrayLike, _DTypeLike, _ShapeLike, _SupportsArrayFunc, _SupportsDType
 from numpy._typing._char_codes import _BoolCodes, _Complex128Codes, _Float64Codes, _IntPCodes
+from numpy.lib._array_utils_impl import normalize_axis_tuple
 
 from ._asarray import require
 from ._ufunc_config import errstate, getbufsize, geterr, geterrcall, setbufsize, seterr, seterrcall
@@ -558,6 +559,8 @@ __all__ = [
     "newaxis",
     "nextafter",
     "nonzero",
+    "normalize_axis_index",
+    "normalize_axis_tuple",
     "not_equal",
     "number",
     "object_",
