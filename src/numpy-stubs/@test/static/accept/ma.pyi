4d5f85180161b38f27b664a65cd2684d546c1c20
Guan Ming(Wesley) Chiu <105915352+guan404ming@users.noreply.github.com>
2025-04-08 02:51:22 +0800

    🚚 port `numpy.ma.min` (#474)
    Co-authored-by: Marco Edward Gorelli <33491632+marcogorelli@users.noreply.github.com>
    Co-authored-by: Joren Hammudoglu <jhammudoglu@gmail.com>

--- src/numpy-stubs/@test/static/accept/ma.pyi
+++ src/numpy-stubs/@test/static/accept/ma.pyi
@@ -1,6 +1,8 @@
+from typing import Any, TypeAlias, TypeVar
 from typing_extensions import assert_type
 
 import numpy as np
+from numpy._typing import _Shape
 
 m: np.ma.MaskedArray[tuple[int], np.dtype[np.float64]]
 
@@ -10,3 +12,14 @@ assert_type(m.dtype, np.dtype[np.float64])
 
 assert_type(int(m), int)
 assert_type(float(m), float)
+
+_ScalarT_co = TypeVar("_ScalarT_co", bound=np.generic, covariant=True)
+MaskedNDArray: TypeAlias = np.ma.MaskedArray[_Shape, np.dtype[_ScalarT_co]]
+
+class MaskedNDArraySubclass(MaskedNDArray[np.complex128]): ...
+
+MAR_b: MaskedNDArray[np.bool]
+MAR_f4: MaskedNDArray[np.float32]
+MAR_i8: MaskedNDArray[np.int64]
+MAR_subclass: MaskedNDArraySubclass
+MAR_1d: np.ma.MaskedArray[tuple[int], np.dtype[Any]]
