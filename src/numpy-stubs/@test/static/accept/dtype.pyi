f8978519c250998e3099eed96352a35a781d4c80
jorenham <jhammudoglu@gmail.com>
2025-04-08 05:28:45 +0200

    ✨ return specialized `numpy.dtypes` instances from `dtype.__new__`

--- src/numpy-stubs/@test/static/accept/dtype.pyi
+++ src/numpy-stubs/@test/static/accept/dtype.pyi
@@ -2,11 +2,10 @@ import ctypes as ct
 from typing_extensions import LiteralString, assert_type
 
 import numpy as np
-from numpy.dtypes import StringDType
 
 dtype_U: np.dtype[np.str_]
 dtype_V: np.dtype[np.void]
-dtype_i8: np.dtype[np.int64]
+dtype_i8: np.dtypes.Int64DType
 
 py_int_co: type[int]
 py_float_co: type[float]
@@ -19,30 +18,30 @@ ct_floating: type[ct.c_float | ct.c_double | ct.c_longdouble]
 ct_number: type[ct.c_uint8 | ct.c_float]
 ct_generic: type[ct.c_bool | ct.c_char]
 
-dt_string: StringDType
+dt_string: np.dtypes.StringDType
 
-assert_type(np.dtype(np.float64), np.dtype[np.float64])
-assert_type(np.dtype(np.float64, metadata={"test": "test"}), np.dtype[np.float64])
-assert_type(np.dtype(np.int64), np.dtype[np.int64])
+assert_type(np.dtype(np.float64), np.dtypes.Float64DType)
+assert_type(np.dtype(np.float64, metadata={"test": "test"}), np.dtypes.Float64DType)
+assert_type(np.dtype(np.int64), np.dtypes.Int64DType)
 
 # String aliases
-assert_type(np.dtype("bool"), np.dtype[np.bool])
-assert_type(np.dtype("int32"), np.dtype[np.int32])
-assert_type(np.dtype("int64"), np.dtype[np.int64])
-assert_type(np.dtype("float32"), np.dtype[np.float32])
-assert_type(np.dtype("float64"), np.dtype[np.float64])
-assert_type(np.dtype("bytes"), np.dtype[np.bytes_])
-assert_type(np.dtype("str"), np.dtype[np.str_])
+assert_type(np.dtype("bool"), np.dtypes.BoolDType)
+assert_type(np.dtype("int32"), np.dtypes.Int32DType)
+assert_type(np.dtype("int64"), np.dtypes.Int64DType)
+assert_type(np.dtype("float32"), np.dtypes.Float32DType)
+assert_type(np.dtype("float64"), np.dtypes.Float64DType)
+assert_type(np.dtype("bytes"), np.dtypes.BytesDType)
+assert_type(np.dtype("str"), np.dtypes.StrDType)
 
 # Python types
-assert_type(np.dtype(bool), np.dtype[np.bool])
-assert_type(np.dtype(int), np.dtype[np.int_])
-assert_type(np.dtype(float), np.dtype[np.float64])
-assert_type(np.dtype(complex), np.dtype[np.complex128])
-assert_type(np.dtype(object), np.dtype[np.object_])
-assert_type(np.dtype(str), np.dtype[np.str_])
-assert_type(np.dtype(bytes), np.dtype[np.bytes_])
-assert_type(np.dtype(memoryview), np.dtype[np.void])
+assert_type(np.dtype(bool), np.dtypes.BoolDType)
+assert_type(np.dtype(int), np.dtypes.Int64DType)
+assert_type(np.dtype(float), np.dtypes.Float64DType)
+assert_type(np.dtype(complex), np.dtypes.Complex128DType)
+assert_type(np.dtype(object), np.dtypes.ObjectDType)
+assert_type(np.dtype(str), np.dtypes.StrDType)
+assert_type(np.dtype(bytes), np.dtypes.BytesDType)
+assert_type(np.dtype(memoryview), np.dtypes.VoidDType)
 
 assert_type(np.dtype(np.signedinteger), np.dtype[np.signedinteger])
 assert_type(np.dtype(np.unsignedinteger), np.dtype[np.unsignedinteger])
@@ -55,37 +54,37 @@ assert_type(np.dtype(np.number), np.dtype[np.number])
 assert_type(np.dtype(np.generic), np.dtype[np.generic])
 
 # char-codes
-assert_type(np.dtype("u1"), np.dtype[np.uint8])
-assert_type(np.dtype("int_"), np.dtype[np.intp])
-assert_type(np.dtype("longlong"), np.dtype[np.longlong])
-assert_type(np.dtype(">g"), np.dtype[np.longdouble])
+assert_type(np.dtype("u1"), np.dtypes.UInt8DType)
+assert_type(np.dtype("int_"), np.dtypes.Int64DType)
+assert_type(np.dtype("longlong"), np.dtypes.Int64DType)
+assert_type(np.dtype(">g"), np.dtypes.LongDoubleDType)
 
 # ctypes
-assert_type(np.dtype(ct.c_bool), np.dtype[np.bool])
-assert_type(np.dtype(ct.c_uint32), np.dtype[np.uint32])
-assert_type(np.dtype(ct.c_ssize_t), np.dtype[np.intp])
-assert_type(np.dtype(ct.c_longlong), np.dtype[np.longlong])
-assert_type(np.dtype(ct.c_double), np.dtype[np.double])
-assert_type(np.dtype(ct.py_object), np.dtype[np.object_])
-assert_type(np.dtype(ct.c_char), np.dtype[np.bytes_])
+assert_type(np.dtype(ct.c_bool), np.dtypes.BoolDType)
+assert_type(np.dtype(ct.c_uint32), np.dtypes.UInt32DType)
+assert_type(np.dtype(ct.c_ssize_t), np.dtypes.Int64DType)
+assert_type(np.dtype(ct.c_longlong), np.dtypes.Int64DType)
+assert_type(np.dtype(ct.c_double), np.dtypes.Float64DType)
+assert_type(np.dtype(ct.py_object), np.dtypes.ObjectDType)
+assert_type(np.dtype(ct.c_char), np.dtypes.BytesDType)
 
 # Special case for None
-assert_type(np.dtype(None), np.dtype[np.float64])
+assert_type(np.dtype(None), np.dtypes.Float64DType)
 
 # Dypes of dtypes
-assert_type(np.dtype(np.dtype(np.float64)), np.dtype[np.float64])
+assert_type(np.dtype(np.dtype(np.float64)), np.dtypes.Float64DType)
 
 # Parameterized dtypes
 assert_type(np.dtype("S8"), np.dtype)
 
 # Void
-assert_type(np.dtype(("U", 10)), np.dtype[np.void])
+assert_type(np.dtype(("U", 10)), np.dtypes.VoidDType)
 
 # StringDType
-assert_type(np.dtype(dt_string), StringDType)
-assert_type(np.dtype("T"), StringDType)
-assert_type(np.dtype("=T"), StringDType)
-assert_type(np.dtype("|T"), StringDType)
+assert_type(np.dtype(dt_string), np.dtypes.StringDType)
+assert_type(np.dtype("T"), np.dtypes.StringDType)
+assert_type(np.dtype("=T"), np.dtypes.StringDType)
+assert_type(np.dtype("|T"), np.dtypes.StringDType)
 
 # Methods and attributes
 assert_type(dtype_U.base, np.dtype)
@@ -100,7 +99,7 @@ assert_type(dtype_U * 1, np.dtype[np.str_])
 assert_type(dtype_U * 2, np.dtype[np.str_])
 
 assert_type(dtype_i8 * 0, np.dtype[np.void])
-assert_type(dtype_i8 * 1, np.dtype[np.int64])
+assert_type(dtype_i8 * 1, np.dtypes.Int64DType)
 assert_type(dtype_i8 * 2, np.dtype[np.void])
 
 assert_type(0 * dtype_U, np.dtype[np.str_])
