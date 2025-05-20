6179c948adb02f5f27f6014b367d37077ac95cac
Joren Hammudoglu <jhammudoglu@gmail.com>
2025-05-20 03:48:37 +0200

    🔨 only delete `__init__.pyi` when "unstubbing" (#570)

--- tool/unstub.py
+++ tool/unstub.py
@@ -1,5 +1,7 @@
 """
-Delete all `.pyi` files in the `numpy` site-packages directory.
+Delete all `__init__.pyi` files in the `numpy` site-packages directory.
+
+This is a workaround for https://github.com/python/mypy/issues/18997 on `mypy<1.16.0`
 
 Run with `uv run tool/unstub.py`.
 """
@@ -10,6 +12,5 @@ from pathlib import Path
 package = Path(sysconfig.get_paths()["purelib"]) / "numpy"
 assert package.is_dir(), package
 
-(package / "py.typed").unlink(missing_ok=True)
-bodycount = sum(not pyi.unlink() for pyi in package.rglob("*.pyi"))  # type: ignore[func-returns-value]
-print(f"{bodycount} files deleted from {package.relative_to(Path.cwd())}/**/*.pyi")
+bodycount = sum(not pyi.unlink() for pyi in package.rglob("__init__.pyi"))  # type: ignore[func-returns-value]
+print(f"{bodycount} files deleted from {package.relative_to(Path.cwd())}")
