219bde30eaaaadb345b267e3724242e27536cf5d
jorenham <jhammudoglu@gmail.com>
2025-12-28 15:22:22 +0100

    👽️ `_core.function_base` sync with upstream

--- src/numpy-stubs/_core/function_base.pyi
+++ src/numpy-stubs/_core/function_base.pyi
@@ -1,232 +1,356 @@
 from _typeshed import Incomplete
-from typing import Literal as L, SupportsIndex, TypeAlias, overload
-from typing_extensions import TypeVar
+from typing import Any, Literal as L, SupportsIndex, TypeVar, overload
 
 import _numtype as _nt
 import numpy as np
-from numpy._typing import DTypeLike, _ArrayLikeFloat_co, _ArrayLikeNumber_co, _DTypeLike
+from numpy._typing import DTypeLike, NDArray, _DTypeLike
 
 __all__ = ["geomspace", "linspace", "logspace"]
 
-###
-
 _ScalarT = TypeVar("_ScalarT", bound=np.generic)
 
-_Device: TypeAlias = L["cpu"]
-
 ###
 
 @overload
 def linspace(
-    start: _ArrayLikeFloat_co,
-    stop: _ArrayLikeFloat_co,
+    start: _nt.CoFloat64_nd,
+    stop: _nt.CoFloat64_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     retstep: L[False] = False,
     dtype: None = None,
     axis: SupportsIndex = 0,
     *,
-    device: _Device | None = None,
-) -> _nt.Array[np.floating]: ...
+    device: L["cpu"] | None = None,
+) -> NDArray[np.float64]: ...
 @overload
 def linspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoFloating_nd,
+    stop: _nt.CoFloating_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     retstep: L[False] = False,
     dtype: None = None,
     axis: SupportsIndex = 0,
     *,
-    device: _Device | None = None,
-) -> _nt.Array[np.inexact]: ...
+    device: L["cpu"] | None = None,
+) -> NDArray[np.float64 | Any]: ...
 @overload
 def linspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.ToComplex128_nd,
+    stop: _nt.CoComplex128_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    retstep: L[False] = False,
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+    *,
+    device: L["cpu"] | None = None,
+) -> NDArray[np.complex128]: ...
+@overload
+def linspace(
+    start: _nt.CoComplex128_nd,
+    stop: _nt.ToComplex128_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    retstep: L[False] = False,
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+    *,
+    device: L["cpu"] | None = None,
+) -> NDArray[np.complex128]: ...
+@overload
+def linspace(
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    retstep: L[False] = False,
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+    *,
+    device: L["cpu"] | None = None,
+) -> NDArray[np.complex128 | Any]: ...
+@overload
+def linspace(
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex,
     endpoint: bool,
     retstep: L[False],
     dtype: _DTypeLike[_ScalarT],
     axis: SupportsIndex = 0,
     *,
-    device: _Device | None = None,
-) -> _nt.Array[_ScalarT]: ...
+    device: L["cpu"] | None = None,
+) -> NDArray[_ScalarT]: ...
 @overload
 def linspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     retstep: L[False] = False,
     *,
     dtype: _DTypeLike[_ScalarT],
     axis: SupportsIndex = 0,
-    device: _Device | None = None,
-) -> _nt.Array[_ScalarT]: ...
+    device: L["cpu"] | None = None,
+) -> NDArray[_ScalarT]: ...
 @overload
 def linspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     retstep: L[False] = False,
     dtype: DTypeLike | None = None,
     axis: SupportsIndex = 0,
     *,
-    device: _Device | None = None,
-) -> _nt.Array[Incomplete]: ...
+    device: L["cpu"] | None = None,
+) -> NDArray[Incomplete]: ...
 @overload
 def linspace(
-    start: _ArrayLikeFloat_co,
-    stop: _ArrayLikeFloat_co,
+    start: _nt.CoFloat64_nd,
+    stop: _nt.CoFloat64_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     *,
     retstep: L[True],
     dtype: None = None,
     axis: SupportsIndex = 0,
-    device: _Device | None = None,
-) -> tuple[_nt.Array[np.floating], np.floating]: ...
+    device: L["cpu"] | None = None,
+) -> tuple[NDArray[np.float64], np.float64]: ...
 @overload
 def linspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoFloating_nd,
+    stop: _nt.CoFloating_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     *,
     retstep: L[True],
     dtype: None = None,
     axis: SupportsIndex = 0,
-    device: _Device | None = None,
-) -> tuple[_nt.Array[np.inexact], np.inexact]: ...
+    device: L["cpu"] | None = None,
+) -> tuple[NDArray[np.floating], np.floating]: ...
 @overload
 def linspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.ToComplex128_nd,
+    stop: _nt.CoComplex128_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    *,
+    retstep: L[True],
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+    device: L["cpu"] | None = None,
+) -> tuple[NDArray[np.complex128], np.complex128]: ...
+@overload
+def linspace(
+    start: _nt.CoComplex128_nd,
+    stop: _nt.ToComplex128_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    *,
+    retstep: L[True],
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+    device: L["cpu"] | None = None,
+) -> tuple[NDArray[np.complex128], np.complex128]: ...
+@overload
+def linspace(
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    *,
+    retstep: L[True],
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+    device: L["cpu"] | None = None,
+) -> tuple[NDArray[np.complex128 | Any], np.complex128 | Any]: ...
+@overload
+def linspace(
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     *,
     retstep: L[True],
     dtype: _DTypeLike[_ScalarT],
     axis: SupportsIndex = 0,
-    device: _Device | None = None,
-) -> tuple[_nt.Array[_ScalarT], _ScalarT]: ...
+    device: L["cpu"] | None = None,
+) -> tuple[NDArray[_ScalarT], _ScalarT]: ...
 @overload
 def linspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     *,
     retstep: L[True],
     dtype: DTypeLike | None = None,
     axis: SupportsIndex = 0,
-    device: _Device | None = None,
-) -> tuple[_nt.Array[Incomplete], Incomplete]: ...
+    device: L["cpu"] | None = None,
+) -> tuple[NDArray[Incomplete], Incomplete]: ...
 
 #
 @overload
 def logspace(
-    start: _ArrayLikeFloat_co,
-    stop: _ArrayLikeFloat_co,
+    start: _nt.CoFloat64_nd,
+    stop: _nt.CoFloat64_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
-    base: _ArrayLikeFloat_co = 10.0,
+    base: _nt.CoFloat64_nd = 10.0,
     dtype: None = None,
     axis: SupportsIndex = 0,
-) -> _nt.Array[np.floating]: ...
+) -> NDArray[np.float64]: ...
 @overload
 def logspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoFloating_nd,
+    stop: _nt.CoFloating_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
-    base: _ArrayLikeNumber_co = 10.0,
+    base: _nt.CoFloating_nd = 10.0,
     dtype: None = None,
     axis: SupportsIndex = 0,
-) -> _nt.Array[np.inexact]: ...
+) -> NDArray[np.float64 | Any]: ...
 @overload
 def logspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.ToComplex128_nd,
+    stop: _nt.CoComplex128_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    base: _nt.CoComplex_nd = 10.0,
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+) -> NDArray[np.complex128]: ...
+@overload
+def logspace(
+    start: _nt.CoComplex128_nd,
+    stop: _nt.ToComplex128_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    base: _nt.CoComplex_nd = 10.0,
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+) -> NDArray[np.complex128]: ...
+@overload
+def logspace(
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    base: _nt.CoComplex_nd = 10.0,
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+) -> NDArray[np.complex128 | Any]: ...
+@overload
+def logspace(
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex,
     endpoint: bool,
-    base: _ArrayLikeNumber_co,
+    base: _nt.CoComplex_nd,
     dtype: _DTypeLike[_ScalarT],
     axis: SupportsIndex = 0,
-) -> _nt.Array[_ScalarT]: ...
+) -> NDArray[_ScalarT]: ...
 @overload
 def logspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
-    base: _ArrayLikeNumber_co = 10.0,
+    base: _nt.CoFloating_nd = 10.0,
     *,
     dtype: _DTypeLike[_ScalarT],
     axis: SupportsIndex = 0,
-) -> _nt.Array[_ScalarT]: ...
+) -> NDArray[_ScalarT]: ...
 @overload
 def logspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
-    base: _ArrayLikeNumber_co = 10.0,
+    base: _nt.CoComplex_nd = 10.0,
     dtype: DTypeLike | None = None,
     axis: SupportsIndex = 0,
-) -> _nt.Array[Incomplete]: ...
+) -> NDArray[Incomplete]: ...
 
 #
 @overload
 def geomspace(
-    start: _ArrayLikeFloat_co,
-    stop: _ArrayLikeFloat_co,
+    start: _nt.CoFloat64_nd,
+    stop: _nt.CoFloat64_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     dtype: None = None,
     axis: SupportsIndex = 0,
-) -> _nt.Array[np.floating]: ...
+) -> NDArray[np.float64]: ...
 @overload
 def geomspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoFloating_nd,
+    stop: _nt.CoFloating_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     dtype: None = None,
     axis: SupportsIndex = 0,
-) -> _nt.Array[np.inexact]: ...
+) -> NDArray[np.float64 | Any]: ...
 @overload
 def geomspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.ToComplex128_nd,
+    stop: _nt.CoComplex128_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+) -> NDArray[np.complex128]: ...
+@overload
+def geomspace(
+    start: _nt.CoComplex128_nd,
+    stop: _nt.ToComplex128_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+) -> NDArray[np.complex128]: ...
+@overload
+def geomspace(
+    start: _nt.ToComplex_nd,
+    stop: _nt.ToComplex_nd,
+    num: SupportsIndex = 50,
+    endpoint: bool = True,
+    dtype: None = None,
+    axis: SupportsIndex = 0,
+) -> NDArray[np.complex128 | Any]: ...
+@overload
+def geomspace(
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex,
     endpoint: bool,
     dtype: _DTypeLike[_ScalarT],
     axis: SupportsIndex = 0,
-) -> _nt.Array[_ScalarT]: ...
+) -> NDArray[_ScalarT]: ...
 @overload
 def geomspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     *,
     dtype: _DTypeLike[_ScalarT],
     axis: SupportsIndex = 0,
-) -> _nt.Array[_ScalarT]: ...
+) -> NDArray[_ScalarT]: ...
 @overload
 def geomspace(
-    start: _ArrayLikeNumber_co,
-    stop: _ArrayLikeNumber_co,
+    start: _nt.CoComplex_nd,
+    stop: _nt.CoComplex_nd,
     num: SupportsIndex = 50,
     endpoint: bool = True,
     dtype: DTypeLike | None = None,
     axis: SupportsIndex = 0,
-) -> _nt.Array[Incomplete]: ...
+) -> NDArray[Incomplete]: ...
 
 #
 def add_newdoc(
-    place: str, obj: str | None, doc: str | tuple[str, str] | list[tuple[str, str]], warn_on_python: bool = True
-) -> None: ...  # undocumented
+    place: str, obj: str, doc: str | tuple[str, str] | list[tuple[str, str]], warn_on_python: bool = True
+) -> None: ...
