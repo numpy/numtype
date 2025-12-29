dd2bb9717e13d35812e9d5bae8e149ab98ae0001
jorenham <jhammudoglu@gmail.com>
2025-12-29 22:51:40 +0100

    🐟 update `pyright` error codes in histogram rejection tests

--- src/numpy-stubs/@test/static/reject/histograms.pyi
+++ src/numpy-stubs/@test/static/reject/histograms.pyi
@@ -4,9 +4,9 @@ import numpy as np
 AR_i8: _nt.Array[np.int64]
 AR_f8: _nt.Array[np.float64]
 
-np.histogram_bin_edges(AR_i8, range=(0, 1, 2))  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]
+np.histogram_bin_edges(AR_i8, range=(0, 1, 2))  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
 
-np.histogram(AR_i8, range=(0, 1, 2))  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]
+np.histogram(AR_i8, range=(0, 1, 2))  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
 
-np.histogramdd(AR_i8, range=(0, 1))  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType]
-np.histogramdd(AR_i8, range=[(0, 1, 2)])  # type: ignore[list-item]  # pyright: ignore[reportArgumentType]
+np.histogramdd(AR_i8, range=(0, 1))  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
+np.histogramdd(AR_i8, range=[(0, 1, 2)])  # type: ignore[list-item]  # pyright: ignore[reportArgumentType, reportCallIssue]
