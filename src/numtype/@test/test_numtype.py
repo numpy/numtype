8b9a6831c172d391d1206cd48f6c9a91bbd6e2a8
jorenham <jhammudoglu@gmail.com>
2025-12-31 17:09:56 +0100

    🔥 remove `numpy.typing` re-exports from `numtype`

--- src/numtype/@test/test_numtype.py
+++ src/numtype/@test/test_numtype.py
@@ -1,19 +1,5 @@
-import pytest
-
 import numpy as np
-import numpy.typing as npt
 import numtype as nt
-
-NPT_DIR = tuple(k for k in npt.__all__ if k != "NBitBase")
-
-
-def test_superset() -> None:
-    assert set(nt.__all__) > set(NPT_DIR)
-
-
-@pytest.mark.parametrize("name", NPT_DIR)
-def test_reexport(name: str) -> None:
-    assert getattr(nt, name) is getattr(npt, name)
 
 
 def test_version() -> None:
--- src/numtype/__init__.py
+++ src/numtype/__init__.py
@@ -1,7 +1,5 @@
 """A superset of `numpy.typing`. Will be expanded in the future."""
 
-from numpy.typing import ArrayLike, DTypeLike, NDArray  # noqa: ICN003
-
 from .version import __version__
 
-__all__ = ["ArrayLike", "DTypeLike", "NDArray", "__version__"]
+__all__ = ["__version__"]
